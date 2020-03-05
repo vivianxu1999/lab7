cdfaf3373d7d6904989483f8f22c3d6aaef04c17
Yiran Xu <vivianxu1999@gmail.com>
2020-03-05 13:48:42 -0800

    complete on Feb19th, forget to push

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(request, response){
-  	response.render('index', projects);
+exports.view = function(request, response) {
+	projects["viewAlt"] = false;
+	console.log(projects);
+	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response) {
+	projects["viewAlt"] = true;
+	console.log(projects);
+	response.render('index', projects);
+}
